d2df5a1bcacf38346d6a13471f71b9068aa09502
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-11-06 08:42:13 -0200

    :tada: Subindo Exemplo

---
+++ ExemploControleCarouselView/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExemploControleCarouselView
+{
+    public partial class MainPage : ContentPage
+    {
+        MainViewModel _vm;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Title = "CarouselView";
+
+            BindingContext = _vm = new MainViewModel();
+        }
+
+        void Handle_PositionSelected(object sender, CarouselView.FormsPlugin.Abstractions.PositionSelectedEventArgs e)
+        {
+            Debug.WriteLine("Posição " + e.NewValue + " Selecionada.");
+        }
+
+        void Handle_Scrolled(object sender, CarouselView.FormsPlugin.Abstractions.ScrolledEventArgs e)
+        {
+            Debug.WriteLine("Scrolled to " + e.NewValue + " percent.");
+            Debug.WriteLine("Direction = " + e.Direction);
+        }
+    }
+}
---
+++ ExemploControleCarouselView/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using Xamarin.Forms;
+
+namespace ExemploControleCarouselView
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel()
+        {
+            Views = new ObservableCollection<View>()
+            {
+                new Xamarin.Forms.Image() { Source = "bertuzzi.jpg",  Aspect = Aspect.AspectFill },
+                new  Xamarin.Forms.Image() { Source = "bertuzzi2.jpg",  Aspect = Aspect.AspectFill },
+                new  Xamarin.Forms.Image() { Source = "bertuzzi3.jpg", Aspect = Aspect.AspectFill }
+            };
+
+            PositionCommand = new Command(() =>
+            {
+                Debug.WriteLine("Posição selecionada.");
+            });
+        }
+
+        ObservableCollection<View> _views;
+        public ObservableCollection<View> Views
+        {
+            set
+            {
+                _views = value;
+                OnPropertyChanged("Views");
+            }
+            get
+            {
+                return _views;
+            }
+        }
+
+        public Command PositionCommand { protected set; get; }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
